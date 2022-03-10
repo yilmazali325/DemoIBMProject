418061d7e418bc7d0cfa13b396b0d6da9f51d6b3
yilmazali325 <yilmazali325@gmail.com>
2022-03-10 09:22:54 -0500

    request mapping added

--- src/main/java/com/example/demo/controller/RegistrationController.java
+++ src/main/java/com/example/demo/controller/RegistrationController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@RestController("/registration")
+@RestController
+@RequestMapping("/registration")
 public class RegistrationController {
 
     public RegistrationService registrationService;
